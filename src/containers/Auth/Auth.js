bb67b7f29230ba4e262f8c186a6fa3e3a2c1f24f
test <willyovcharuk1994@gmail.com>
2020-03-29 14:56:38 +0300

    add page 'create test'(form, inputs, select, buttons)

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import is from 'is_js'
 export default class Auth extends Component {
 
     state = {
+        isFormValid: false,
         formControls: {
             email: {
                 value: '',
@@ -83,9 +84,15 @@ export default class Auth extends Component {
         control.valid = this.validateControl(control.value, control.validation)
 
         formControls[controlName] = control
+
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
         
         this.setState({
-            formControls
+            formControls, isFormValid
         })
     }
 
@@ -121,6 +128,7 @@ export default class Auth extends Component {
                         <Button 
                             type='success' 
                             onClick={this.loginHandler}
+                            disabled={!this.state.isFormValid}
                         >
                             Sign in
                         </Button>
@@ -128,6 +136,7 @@ export default class Auth extends Component {
                         <Button 
                             type='primary' 
                             onClick={this.registerHandler}
+                            disabled={!this.state.isFormValid}
                         >
                             Sign up
                         </Button>
