de6a5a60295bc2d9724c3a75468be09329a32dec
test <willyovcharuk1994@gmail.com>
2020-03-29 20:33:04 +0300

    add post to db firebase

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -5,6 +5,7 @@ import Input from '../../components/UI/Input/Input'
 import Select from '../../components/UI/Select/Select'
 import {createControl, validate, validateForm} from '../../form/formFramework'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
+import axious from 'axios'
 
 function createOptionControl(number) {
     return createControl({
@@ -71,10 +72,31 @@ export default class QuizCreator extends Component {
 
     }
 
-    createQuizHandler = (event) => {
+    createQuizHandler = async(event) => {
         event.preventDefault();
+            
+        try {
+            const response = await axious.post('https://react-quiz-a51e3.firebaseio.com/quizes.json', this.state.quiz)
+            console.log(response.data)
 
-        console.log(this.state.quiz)
+            this.setState({
+                quiz: [],
+                isFormValid: false,
+                rightAnswerId: 1,
+                formControls: createFormControls(),
+            })
+
+        } catch (e) {
+            console.log(e)
+        }
+
+        // axious.post('https://react-quiz-a51e3.firebaseio.com/quizes.json', this.state.quiz)
+        //     .then(response => {
+        //         console.log(response)
+        //     })
+        //     .catch(error => console.log(error))
+
+        // console.log(this.state.quiz)
 
     }
 
--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './QuizList.module.css'
 import {NavLink} from 'react-router-dom'
+import axious from 'axios'
 
 export default class QuizList extends Component {
 
@@ -19,6 +20,11 @@ export default class QuizList extends Component {
         })
     }
 
+    componentDidMount() {
+        axious.get('https://react-quiz-a51e3.firebaseio.com/quiz.json').then(response => {
+            console.log(response)
+        })
+    }
 
     render() {
         return(
