c64522f5d3a61c441504e6f73509c9701d8a3ba3
test <willyovcharuk1994@gmail.com>
2020-04-30 18:02:28 +0300

    page auth.js refactered

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,170 +3,160 @@ import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
-import axios from 'axios'
+import {connect} from 'react-redux'
+import {auth} from '../../store/actions/auth'
 
-export default class Auth extends Component {
+class Auth extends Component {
 
-    state = {
-        isFormValid: false,
-        formControls: {
-            email: {
-                value: '',
-                type: 'email',
-                label: 'Email',
-                errorMessage: 'type correct email',
-                valid: 'false',
-                touched: false,
-                validation: {
-                    required: true,
-                    email: true
-                }
-
-            },
-            password: {
-
-                value: '',
-                type: 'password',
-                label: 'Password',
-                errorMessage: 'type correct password',
-                valid: 'false',
-                touched: false,
-                validation: {
-                    required: true,
-                    minLength: 6
-                }
-
-            },
+  state = {
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
         }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
+
+  loginHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      true
+    )
+  }
+
+  registerHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      false
+    )
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
+  validateControl(value, validation) {
+    if (!validation) {
+      return true
     }
 
-    loginHandler = async() => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-        try{
-            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key       AIzaSyCqS-nBVhmN7o95wuvJSSA8hTts3aR9CF4', authData)
-            console.log(response.data)
-        } catch (e) {
-            console.log(e)
-        }
+    let isValid = true
 
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid
     }
 
-    registerHandler = async() => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-        try{
-            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCqS-nBVhmN7o95wuvJSSA8hTts3aR9CF4', authData)
-            console.log(response.data)
-        } catch (e) {
-            console.log(e)
-        }
-        
-        
+    if (validation.email) {
+      isValid = is.email(value) && isValid
     }
 
-    submitHandler = (event) => {
-        event.preventDefault();
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid
     }
 
-    validateControl(value, validation) {
-        if(!validation) {
-            return true
-        }
+    return isValid
+  }
 
-        let isValid = true
+  onChangeHandler = (event, controlName) => {
+    const formControls = { ...this.state.formControls }
+    const control = { ...formControls[controlName] }
 
-        if (validation.required) {
-            isValid = value.trim() !== '' && isValid
-        }
+    control.value = event.target.value
+    control.touched = true
+    control.valid = this.validateControl(control.value, control.validation)
 
-        if (validation.email) {
-            isValid = is.email(value) && isValid
-        }
+    formControls[controlName] = control
 
-        if (validation.minLength) {
-            isValid = value.length >= validation.minLength && isValid
-        }
+    let isFormValid = true
 
-        return isValid
-    }
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid
+    })
 
-    onChangeHendler = (event, controlName) => {
-        // console.log(`${controlName}: `, event.target.value)
+    this.setState({
+      formControls, isFormValid
+    })
+  }
 
-        const formControls = {...this.state.formControls}
-        const control = { ...formControls[controlName] }
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)}
+        />
+      )
+    })
+  }
 
-        control.value = event.target.value
-        control.touched = true
-        control.valid = this.validateControl(control.value, control.validation)
+  render() {
+    return (
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
 
-        formControls[controlName] = control
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
 
-        let isFormValid = true
+            { this.renderInputs() }
 
-        Object.keys(formControls).forEach(name => {
-            isFormValid = formControls[name].valid && isFormValid
-        })
-        
-        this.setState({
-            formControls, isFormValid
-        })
-    }
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Войти
+            </Button>
 
-    renderInputs() {
-        return Object.keys(this.state.formControls).map((controlName, index) => {
-            const control = this.state.formControls[controlName]
-            return (
-                <Input 
-                    key={controlName + index}
-                    type={control.type}
-                    value={control.value}
-                    valid={control.valid}
-                    touched={control.touched}
-                    label={control.label}
-                    shouldValidate={!!control.validation}
-                    errorMessage={control.errorMessage}
-                    onChange={event => this.onChangeHendler(event, controlName)}
-                />
-            )
-        })
-    }
-
-    render() {
-        return(
-            <div className={classes.Auth}>
-                <div>
-                    <h1>Authorization</h1>
-
-                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-    
-                        {this.renderInputs()}
-
-                        <Button 
-                            type='success' 
-                            onClick={this.loginHandler}
-                            disabled={!this.state.isFormValid}
-                        >
-                            Sign in
-                        </Button>
-
-                        <Button 
-                            type='primary' 
-                            onClick={this.registerHandler}
-                            disabled={!this.state.isFormValid}
-                        >
-                            Sign up
-                        </Button>
-                    </form>
-                </div> 
-            </div>
-        )
-    }
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Auth)
---
+++ src/store/actions/auth.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import {AUTH_SUCCESS, AUTH_LOGOUT} from './actionTypes'
+
+export function auth(email, password, isLogin) {
+    return async dispatch => {
+        const authData = {
+            email, password,
+            returnSecureToken: true
+        }
+
+        let url = 'https://identitytoolkit.googleapis.com/v1/relyingparty/signupNewUser?key=AIzaSyCqS-nBVhmN7o95wuvJSSA8hTts3aR9CF4';
+
+        if (isLogin) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCqS-nBVhmN7o95wuvJSSA8hTts3aR9CF4'
+        }
+        const response = await axios.post(url, authData)
+        const data = response.data
+
+        const expirationDate = new Date(new Date.getTime() + data.expiresIn * 1000)
+
+        localStorage.setItem('token', data.IsToken)
+        localStorage.setItem('userId', data.localId)
+        localStorage.setItem('expirationDate', data.expirationDate)
+
+        dispatch(authSuccess(data.idToken))
+        dispatch(autoLogout(data.expiresIn))
+        // try{
+        //     const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?keyAIzaSyCqS-nBVhmN7o95wuvJSSA8hTts3aR9CF4', authData)
+        //     console.log(response.data)
+        // } catch (e) {
+        //     console.log(e)
+        // }
+    }
+}
+
+export function autoLogout(time){
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout())
+        }, time * 1000)
+    }
+}
+
+export function logout() {
+    localStorage.removeItem('token')
+    localStorage.removeItem('userId')
+    localStorage.removeItem('expirationDate')
+    return {
+        type: AUTH_LOGOUT
+    }
+}
+
+export function authSuccess(token) {
+    return {
+        type: AUTH_SUCCESS,
+        token
+    }
+}
